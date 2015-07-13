e4152622a3991ca5406e871b1d95fc0e4cc8d646
oleg <chezar1995@gmail.com>
2015-07-13 10:35:55 +0300

    upgrade setup

--- setup.py
+++ setup.py
@@ -6,26 +6,15 @@ INSTALL_REQUIRES = [
 ]
 
 setup(
-    name='turbosmsua3',
-    version='0.1dev',
-    packages=['turbosmsua3',],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.md').read(),
-
-    install_requires = INSTALL_REQUIRES,
-)
-
-from distutils.core import setup
-
-setup(
   name = 'turbosmsua',
-  packages = ['turbosmsua3'], # this must be the same as the name above
+  packages = ['turbosmsua'],
   version = '0.1',
   description = 'Client for https://turbosms.ua',
   author = 'Serbokryl Oleg',
   author_email = 'chezar1995@gmail.com',
-  url = 'https://github.com/Krokop/turbosmsua3', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
-  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  url = 'https://github.com/Krokop/turbosmsua', 
+  download_url = 'https://github.com/Krokop/turbosmsua/tarball/0.1',
+  keywords = ['turbosmsua', 'sms ua'],
   classifiers = [],
+  install_requires = INSTALL_REQUIRES,
 )
